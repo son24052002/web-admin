900cc406ff2c9215fc1a7c059d9a7d583e9c626f
son24052002 <son240502@gmail.com>
2024-01-12 11:35:15 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <side-bar></side-bar>
+    <top-bar></top-bar>
+  </v-app>
 </template>
 
+<script>
+import SideBar from './components/SideBar.vue'
+import TopBar from './components/TopBar.vue'
+export default {
+  components: { SideBar, TopBar },
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
